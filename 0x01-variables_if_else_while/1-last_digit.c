7acc1f288a2f0d89d77756c69b863f79a509c027
ortegawork <ortagaworkineh11@gmail.com>
2022-06-06 14:11:41 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,24 +27,18 @@ int main(void)
 
 
 		ld = n % 10;
-
-						if (ld == 0)
-
-						{
-							printf("Last digit of %i is %i and is 0\n", n, ld);
-						}
-
-						else if (ld < 6 && ld != 0)
-
-						{
-							printf("Last digit of %i is %i and is less than 6 and not 0\n", n, ld);
-						}
-
-						if (ld > 5)
-
-						{
-							printf("Last digit of %i is %i and is greater than 5\n", n, ld);
-						}
-						return (0);
+		if (ld == 0)
+		{
+			printf("Last digit of %i is %i and is 0\n", n, ld);
+		}
+		else if (ld < 6 && ld != 0)
+		{
+			printf("Last digit of %i is %i and is less than 6 and not 0\n", n, ld);
+		}
+		if (ld > 5)
+		{
+			printf("Last digit of %i is %i and is greater than 5\n", n, ld);
+		}
+		return (0);
 
 }
