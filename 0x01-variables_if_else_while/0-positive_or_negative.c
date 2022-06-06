ba783297e4c7797739b0d7d12942122fe42e6669
ortegawork <ortagaworkineh11@gmail.com>
2022-06-06 13:02:04 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,30 +23,18 @@ int main(void)
 
 
 					if (n == 0)
-
-							{
-
-										printf("%d is zero\n", n);
-
-											}
-
-						else if (n < 0)
-
-								{
-
-											printf("%d is negative\n", n);
-
-												}
-
-							else
-
-									{
-
-												printf("%d is positive\n", n);
-
-													}
-
-								return (0);
+					{
+						printf("%d is zero\n", n);
+					}
+					else if (n < 0)
+					{
+						printf("%d is negative\n", n);
+					}
+					else
+					{
+						printf("%d is positive\n", n);
+					}
+							return (0);
 
 }
 
