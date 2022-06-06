d0d64bbef3918652a7913a19383f3bb1175c467a
ortegawork <ortagaworkineh11@gmail.com>
2022-06-06 06:04:31 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,12 @@
 
  int main(void)
 
+
+/**
+ *  *   * main - Entry point
+ *   *   *
+ *    *   * Return: Always 0 (Success)
+ *     **/
 {
 	puts("\'Programming is like building a multilingual puzzle");
 
