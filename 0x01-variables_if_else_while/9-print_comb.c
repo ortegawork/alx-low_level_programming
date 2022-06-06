52b54fd5839377f3dc82f2c664d3aa33543d2243
ortegawork <ortagaworkineh11@gmail.com>
2022-06-06 15:14:45 -0700

    prints all the possibile combinations of a single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *  *   * main - Entry point
+ *   *   *
+ *    *   * Return: Always 0 (Success)
+ *     **/
+
+int main(void)
+
+{
+
+		int numb;
+
+
+
+			for (numb = '0' ; numb <= '9' ; numb++)
+			{
+				putchar(numb);
+				if (num != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				putchar('\n');
+				return (0);
+
+}
