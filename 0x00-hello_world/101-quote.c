d25fcdd87c0201ea7cd4c0abb2ffbe2062cf43aa
ortegawork <ortagaworkineh11@gmail.com>
2022-06-06 07:32:52 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include <unistd.h>
 
 /**
@@ -10,7 +9,6 @@
 
 int main(void)
 {
-
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, str, 59);
 	return (1);
