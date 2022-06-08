6c7fbc3adc4a2413e82f36c750f382af3ce1eb2e
ortegawork <ortagaworkineh11@gmail.com>
2022-06-08 12:01:04 -0700

    print the alphabet in lowercase

---
+++ 1-alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *   *   * print_alphabet - Entry point
+ *    *   *
+ *     *    * Return: Always 0 (Success)
+ *      **/
+
+void print_alphabet(void)
+
+{
+
+		int alpha;
+
+
+
+			alpha = 'a';
+
+
+
+				while (alpha <= 'z')
+
+						{
+							_putchar(alpha);
+							alpha++
+						}
+				_putch('\n');
+}
