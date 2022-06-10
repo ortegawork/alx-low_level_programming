1e78cf9290ad53ac02d78192a7f81e0e77fbe1d8
ortegawork <ortagaworkineh11@gmail.com>
2022-06-10 15:47:57 -0700

    task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,14 +10,12 @@ void print_most_numbers(void)
 {
 
 		int numb;
-
-
-
-			for (numb = 48 ; numb <= 57 ; numb++)
+		for (numb = 48 ; numb <= 57 ; numb++)
+		{
+			if (numb != 50 && numb != 52)
 			{
-				if (numb != 50 && numb != 52)
-				{
-					_putchar(numb);
-				}
-				_putchar('\n');
+				_putchar(numb);
+			}
+		}
+		_putchar('\n');
 }
