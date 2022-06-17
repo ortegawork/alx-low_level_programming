31ee9955cae9d0a63dff643eb78ee71dd83e67e0
ortegawork <ortagaworkineh112gmail.com>
2022-06-17 08:29:51 -0700

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,10 +3,10 @@
 
 
 /**
- *  *   * leet - function that encodes a string into 1337.
- *   *   * @s: Pointer to Char
- *    *   * Return: char.
- *     **/
+ *   *   * leet - function that encodes a string into 1337.
+ *    *   * @s: Pointer to Char
+ *     *   * Return: char.
+ *      **/
 
 
 
@@ -16,23 +16,24 @@ char *leet(char *s)
 
 		int i = 0;
 
-		int j = 0;
+			int j = 0;
 
-		char *l = "aAeEoOlLtT";
+				char *l = "aAeEoOlLtT";
 
-		char *n = "4433001177";
-		
-		
-		      while (*(s + i) != '\0')
-		      {
-			      while (*(l + j) != '\0')
-			      {
-				      if (*(s + i) == *(l + j))
-					      *(s + i) = *(n + j)
-						      j++;
-			      }
-			      j = 0;
-			      i++;
-		      }
-		      return (s);
+					char *n = "4433001177";
+
+
+
+						while (*(s + i) != '\0')
+						{
+							while (*(l + j) != '\0')
+							{
+								if (*(s + i) == *(l + j))
+									*(s + i)  = *(n + j);
+								j++;
+							}
+							j = 0;
+							i++;
+						}
+						return (s);
 }
